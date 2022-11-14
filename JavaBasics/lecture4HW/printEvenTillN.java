cb7bb7e984102bc72e00a56040e5194ed614823c
shivanshu1177 <shivanshu1177@chitkara.edu.in>
2022-11-14 19:24:41 +0530

    couldent input

---
+++ JavaBasics/lecture4HW/printEvenTillN.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class printEvenTillN {
+    public static void main(String[] args) {
+        int n = 20;
+        for(int i = 2; i<=n; i=i+2){
+            System.out.println(i);
+        }
+
+    }
+}
