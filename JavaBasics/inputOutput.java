1eb6b4184284044721c0b4cb185bac2d7c4b8fe2
shivanshu1177 <shivanshu1177@chitkara.edu.in>
2022-11-14 21:18:00 +0530

    menu driven program

--- JavaBasics/inputOutput.java
+++ JavaBasics/inputOutput.java
@@ -1,10 +1,31 @@
-
-
+//menu driven program
+import java.util.Scanner;
 public class inputOutput {
 
     public static void main(String[] args) {
-        System.out.println("This is how we print");
+        System.out.println("Enter 1 or 0: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a == 1){
+            System.out.println("Enter marks(Out of 100): ");
+            int m = sc.nextInt();
+            if (m >= 90) {
+                System.out.println("This is Good");
+
+            }
+            else if(m <= 89 && m>=60){
+                System.out.println("This is alse Good");
+            }
+            else{
+                System.out.println("This is good as well");
+            }
+
+        }
+        else{
+            System.out.println("thank you");
+        }
         
+
     
         
     }
